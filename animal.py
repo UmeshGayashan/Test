e80404593b6dbae468cf8b4333463e15c8fc290e
UmeshGayashan <umeshgayashan099@gmail.com>
2024-02-16 15:23:56 +0530

    Dog function is added

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def dog():
+    print("Baw")
+
 def default():
     print("Hello")
 
 def main():
-    default()
+    if sys =.argv[1]=='dog':
+        dog()
+    else:
+        default()
 
 if _name_=='_main_':
     main()
